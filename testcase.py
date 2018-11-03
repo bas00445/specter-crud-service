141674bf62dd003e5d0d2aeaeb85575729660cd3
sivutzaza@outlook.com <sivutzaza@outlook.com>
2018-11-03 21:41:49 +0700

    Bright try v.5.3

--- testcase.py
+++ testcase.py
@@ -66,10 +66,37 @@ driver.get("http://www.python.org")
 '''
 
 # Firefox
-from selenium.webdriver import Firefox
+ffrom selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
 
-options = Options()
-options.add_argument('-headless')
-firefox = Firefox(firefox_options=options)
-driver.get("http://www.python.org")
+if __name__ == "__main__":
+    options = Options()
+    options.add_argument('-headless')
+    driver = Firefox(executable_path='geckodriver', firefox_options=options)
+    wait = WebDriverWait(driver, timeout=10)
+    driver.get('http://www.google.com')
+    wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
+    wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
+    print(driver.page_source)
+    driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
