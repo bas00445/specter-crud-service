13e90a957512c28bc5bf1398bec7d72e04f62bec
sivutzaza@outlook.com <sivutzaza@outlook.com>
2018-11-03 21:45:05 +0700

    Bright try v.5.5

--- testcase.py
+++ testcase.py
@@ -66,7 +66,7 @@ driver.get("http://www.python.org")
 '''
 
 # Firefox
-ffrom selenium.webdriver import Firefox
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
