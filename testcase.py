ed9d025253a3886874e4a8b37c66525cd40d789c
sivutzaza@outlook.com <sivutzaza@outlook.com>
2018-11-10 17:11:47 +0700

    Back to basic v.4

--- testcase.py
+++ testcase.py
@@ -65,7 +65,7 @@ driver = webdriver.Chrome(chrome_options = options,executable_path=chromedriver)
 driver.get("http://www.python.org")
 '''
 
-
+'''
 # Firefox
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
@@ -79,13 +79,14 @@ options.add_argument('-headless')
 driver = Firefox(firefox_options=options)
 wait = WebDriverWait(driver, timeout=10)
 driver.get('http://www.google.com')
+'''
 
-
+'''
 wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
 wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
 print(driver.page_source)
 driver.quit()
-
+'''
 
 '''
 # Firefox v.2
